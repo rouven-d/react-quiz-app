877b6ac44b5b4b24f6b927a7a692c052ce2ff00b
Rouven <rouven.daunke@googlemail.com>
2021-06-06 12:24:31 +0200

    Setup React App and basic site architecture with react router, css stylesheet added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Game from "./components/Game";
+import HighScores from "./components/HighScores";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+      </Router>
     </div>
   );
 }
