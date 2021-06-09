de0ceef66644db1a66acf29eb79ca19f6e7fd9c9
Rouven <rouven.daunke@googlemail.com>
2021-06-09 20:28:46 +0200

    added function to change questions, validate answers, tracking of correct answers using state of bonus

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,6 +9,8 @@ export default class Game extends Component {
     this.state = {
       questions: null,
       currentQuestion: null,
+      loading: true,
+      score: 0,
     };
   }
 
@@ -18,24 +20,57 @@ export default class Game extends Component {
     try {
       const questions = await loadQuestions();
       //Updating the state with the questions and the currentQuestion set to the index [0] of the questions array
-      this.setState({ questions: questions, currentQuestion: questions[0] });
-      console.log(questions);
+      this.setState(
+        {
+          questions,
+        },
+        () => {
+          this.changeQuestion();
+        }
+      );
     } catch (err) {
       console.error(err);
     }
   }
 
+  changeQuestion = (bonus = 0) => {
+    //get a random index of a question
+    const randomQuestionIndex = Math.floor(
+      Math.random() * this.state.questions.length
+    );
+    //set the current question to the question at that random index
+    const currentQuestion = this.state.questions[randomQuestionIndex];
+    //remove that question from the questions going forward
+    const remainingQuestions = [...this.state.questions];
+    remainingQuestions.splice(randomQuestionIndex, 1);
+    //Update the state to reflect these changes
+    //Added score to the state to keep track of the user's score for correct answers
+    this.setState((prevState) => ({
+      questions: remainingQuestions,
+      currentQuestion,
+      loading: false,
+      score: (prevState.score += bonus),
+    }));
+    console.log(this.state.score);
+  };
+
+  //Here we use the && operator to only render the Question component with
+  //the state of currentQuestion when React actually has a state with the
+  //current question
+  //Meaning only if the Fetch API was successfull and
+  //updated the state!
   render() {
     return (
       <>
-        //Here we use the && operator to only render the Question component with
-        the state of currentQuestion when React actually has a state with the
-        current question //Meaning only if the Fetch API was successfull and
-        updated the state!
-        {this.state.currentQuestion && (
-          <Question question={this.state.currentQuestion} />
+        {this.state.loading && <div id="loader" />}
+        {!this.state.loading && this.state.currentQuestion && (
+          <Question
+            question={this.state.currentQuestion}
+            changeQuestion={this.changeQuestion}
+          />
         )}
       </>
     );
   }
 }
+//Displays the loading icon div only while loading=true, else, if false, renders the question!
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Question({ question }) {
+export default function Question({ question, changeQuestion }) {
+  //Setting states to track user's interaction with the answer options
+  const [classToApply, setClassToApply] = useState("");
+  const [selectedAnswer, setSelectedAnswer] = useState(-1);
+  const [answering, setAnswering] = useState(false);
+
+  const checkAnswer = (selectedAnswer) => {
+    if (answering) return;
+
+    //Check if the selected answer is the right answer (question.answer)
+    setAnswering(true);
+    setSelectedAnswer(selectedAnswer);
+    //Applies a class depending on if the selectedAnswer is equal to the correct answer
+    const classToApply =
+      selectedAnswer === question.answer ? "correct" : "incorrect";
+    setClassToApply(classToApply);
+    //Applies a bonus score if the correct answer was chosen
+    const bonus = selectedAnswer === question.answer ? 10 : 0;
+    setSelectedAnswer(selectedAnswer);
+    setAnswering(true);
+
+    setTimeout(() => {
+      setSelectedAnswer(-1);
+      setAnswering(false);
+      changeQuestion(bonus);
+    }, 1000);
+  };
+
   return (
     <div>
-      //Dangerously setting html passes in a value that might be effected by
-      encoding issues and gets rid of these issues!
       <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
       {question.answerChoices.map((choice, index) => (
-        <div key={index} className="choice-container">
+        <div
+          key={index}
+          className={`choice-container ${
+            selectedAnswer === index && classToApply
+          }`}
+          onClick={() => checkAnswer(index)}
+        >
           <p className="choice-prefix">{index + 1}</p>
           <p
             className="choice-text"
@@ -18,3 +49,5 @@ export default function Question({ question }) {
     </div>
   );
 }
+//Dangerously setting html passes in a value that might be effected by
+//encoding issues and gets rid of these issues!
