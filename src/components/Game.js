2776c2684c77334e382d5e15e8b24c8587c27a37
Rouven <rouven.daunke@googlemail.com>
2021-06-12 16:51:32 +0200

    refactored game.js into react hooks, finished high score form and did some formatting

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,101 +1,93 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Question from "./Question";
 import { loadQuestions } from "../helpers/QuestionsHelper";
 import HUD from "./HUD";
 import SaveScoreForm from "./SaveScoreForm";
 
-export default class Game extends Component {
+export default function Game({ history }) {
   //Defining the state for the questions
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: null,
-      currentQuestion: null,
-      loading: true,
-      score: 0,
-      questionNumber: 0,
-      done: false,
-    };
-  }
 
-  //componentDidMount is a function that gets triggered when the application is loaded i.e. ready to go
-  async componentDidMount() {
-    //Here we know await the function defined in the QuestionsHelper file that we imported above!
-    try {
-      const questions = await loadQuestions();
-      //Updating the state with the questions and the currentQuestion set to the index [0] of the questions array
-      this.setState(
-        {
-          questions,
-        },
-        () => {
-          this.changeQuestion();
-        }
-      );
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [score, setScore] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    loadQuestions()
+      .then((questions) => setQuestions(questions))
+      .catch((err) => console.error(err));
+  }, []);
 
   //This function handles the push back to the home page once a user has saved his/her score
-  scoreSaved = () => {
-    this.props.history.push("/");
+  const scoreSaved = () => {
+    history.push("/");
   };
 
-  changeQuestion = (bonus = 0) => {
-    //Defining the state of done to true when no questions are left in the question array!
-    //Makes sure the bonus is added for the last question in the array!
-    if (this.state.questions.length === 0) {
-      this.setState((prevState) => ({
-        done: true,
-        // score: prevState.score + bonus,
-      }));
+  const changeQuestion = useCallback(
+    (bonus = 0) => {
+      //Defining the state of done to true when no questions are left in the question array!
+      //Makes sure the bonus is added for the last question in the array!
+      if (questions.length === 0) {
+        setDone(true);
+        return setScore(score + bonus);
+      }
+
+      //get a random index of a question
+      const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+      //set the current question to the question at that random index
+      const currentQuestion = questions[randomQuestionIndex];
+      //remove that question from the questions going forward
+      const remainingQuestions = [...questions];
+      remainingQuestions.splice(randomQuestionIndex, 1);
+      //Update the state to reflect these changes
+      //Added score to the state to keep track of the user's score for correct answers
+
+      setQuestions(remainingQuestions);
+      setCurrentQuestion(currentQuestion);
+      setLoading(false);
+      setScore(score + bonus);
+      setQuestionNumber(questionNumber + 1);
+    },
+    [
+      score,
+      questionNumber,
+      questions,
+      setQuestions,
+      setLoading,
+      setCurrentQuestion,
+      setQuestionNumber,
+    ]
+  );
+
+  useEffect(() => {
+    if (!currentQuestion && questions.length) {
+      changeQuestion();
     }
-
-    //get a random index of a question
-    const randomQuestionIndex = Math.floor(
-      Math.random() * this.state.questions.length
-    );
-    //set the current question to the question at that random index
-    const currentQuestion = this.state.questions[randomQuestionIndex];
-    //remove that question from the questions going forward
-    const remainingQuestions = [...this.state.questions];
-    remainingQuestions.splice(randomQuestionIndex, 1);
-    //Update the state to reflect these changes
-    //Added score to the state to keep track of the user's score for correct answers
-    this.setState((prevState) => ({
-      questions: remainingQuestions,
-      currentQuestion,
-      loading: false,
-      score: (prevState.score += bonus),
-      questionNumber: prevState.questionNumber + 1,
-    }));
-  };
+  }, [currentQuestion, questions, changeQuestion]);
 
   //Here we use the && operator to only render the Question component with
   //the state of currentQuestion when React actually has a state with the
   //current question
   //Meaning only if the Fetch API was successfull and
   //updated the state!
-  render() {
-    const { loading, done, score, currentQuestion, questionNumber } =
-      this.state;
-    return (
-      <>
-        {loading && !done && <div id="loader" />}
+  // const { loading, done, score, currentQuestion, questionNumber } = this.state;
+  return (
+    <>
+      {loading && !done && <div id="loader" />}
 
-        {!done && !loading && currentQuestion && (
-          <div>
-            <HUD score={score} questionNumber={questionNumber} />
-            <Question
-              question={currentQuestion}
-              changeQuestion={this.changeQuestion}
-            />
-          </div>
-        )}
-        {done && <SaveScoreForm score={score} scoreSaved={this.scoreSaved} />}
-      </>
-    );
-  }
+      {!done && !loading && currentQuestion && (
+        <div>
+          <HUD score={score} questionNumber={questionNumber} />
+          <Question
+            question={currentQuestion}
+            changeQuestion={changeQuestion}
+          />
+        </div>
+      )}
+      {done && <SaveScoreForm score={score} scoreSaved={scoreSaved} />}
+    </>
+  );
 }
-//Displays the loading icon div only while loading=true, else, if false, renders the question! And only while we are not done!
+//Displays the loading icon div only while loading=true, else, if false, renders the question! And only while we are not done
--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useFirebase } from "./Firebase/FirebaseContext";
+import { Link } from "react-router-dom";
 
 export default function HighScores() {
+  const firebase = useFirebase();
+  //React Hook to keep track of the scores
+  const [scores, setScores] = useState([]);
+  //React Hook for Loading
+  const [loading, setLoading] = useState(true);
+
+  //Get all the highscores saved in firebase once the components are loaded
+  // Snapshot is what firebase returns as data, we can then capture the value of the snapshot
+  useEffect(() => {
+    firebase.scores().once("value", (snapshot) => {
+      const data = snapshot.val();
+      //Operation to format the data from firebase using the function below
+      const sortedScores = formatScoreData(data);
+      setScores(sortedScores);
+      setLoading(false);
+    });
+  }, [firebase]);
+
+  const formatScoreData = (firebaseScores) => {
+    const scores = [];
+    //Takes the key from the firebase data and established key with the firebase key value as a object property then pushed to the scores array
+    for (let key in firebaseScores) {
+      const val = firebaseScores[key];
+      val["key"] = key;
+      scores.push(val);
+    }
+    //Here we sort the data by the size of the score with the highest score on top
+    //We solice the scores aaray to only give out the first 10 values
+    return scores
+      .sort((score1, score2) => score2.score - score1.score)
+      .slice(0, 10);
+  };
+
   return (
     <>
-      <h1>High Scores</h1>
+      {loading && <div id="loader"></div>}
+      {!loading && (
+        <>
+          <h1>High Scores</h1>
+          <div className="highScoresList">
+            {scores.map((record, index) => {
+              return (
+                <div key={index} className="choice-container">
+                  <p className="scorer-name">{record.name}</p>
+                  <p key={record.key} className="high-score">
+                    {record.score}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          <Link to="/" className="hsbtn">
+            Go Home
+          </Link>
+        </>
+      )}
     </>
   );
 }
